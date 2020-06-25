5ce68d77efa90d1a7e998b272f766e32b73b3b8d
Rijk-linux <20376316@sun.ac.za>
2020-06-25 11:45:17 +0200

    Initial layout with 2 dice

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,49 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  return runApp(
-    MaterialApp(
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
           title: Text('Dicee'),
+          elevation: 0.0,
+          centerTitle: true,
           backgroundColor: Colors.red,
         ),
         body: DicePage(),
       ),
-    ),
-  );
+    );
+  }
 }
 
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.only(left: 10),
+              child: Image.asset('images/dice1.png'),
+            ),
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.only(right: 10),
+              child: Image.asset('images/dice1.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
